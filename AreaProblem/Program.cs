a1628e7828abd6d284e9e35b40315c710404c3ce
ashefa-24 <ashefanusrath24@gmail.com>
2022-02-16 13:40:59 +0000

    AreaProblem

---
+++ AreaProblem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AreaProblem
+{
+    class Program
+    {
+        static void Rectangle()
+        {
+            int length, breadth, result;
+            length = 2;
+            breadth = 4;
+            result = length * breadth;
+            Console.WriteLine("Area of the rectangle = " + result);
+        }
+        static void Circle()
+        {
+            double res;
+            res = 3.14 * 5 * 5;
+            Console.WriteLine("Area of the circle = " + res);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Area of Rectangle");
+            Rectangle();
+            Console.WriteLine("Area of Circle");
+            Circle();
+        }
+    }
+}
